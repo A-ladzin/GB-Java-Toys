289309fdb1588e519bd0f56e74bca58eea2f9380
Alexander <a.ladzin1@gmail.com>
2024-02-28 14:25:02 +0300

    update

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) {
 
 
-        ToysMachine tots = new ToysMachine(100);
+        ToysMachine tots = new ToysMachine(256);
 //
         tots.put("Свинка",0.1f);
         tots.put("Зайчик", 0.2f);
